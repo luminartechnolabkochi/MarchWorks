997a07364414c008d5e1868b775eaa589578ec89
sajay <sajaykannan10@gmail.com>
2025-06-19 08:30:20 +0530

    latest

--- DSA/merge_intervals.py
+++ DSA/merge_intervals.py
@@ -17,3 +17,18 @@ source_word="silents"
 # healthify
 
 # kangaro
+
+
+
+
+employees={
+    "emp1":{"code":100,"name":"ravi","department":"hr","salary":25000,"exp":1},
+     "emp2":{"code":101,"name":"vijil","department":"hr","salary":24000,"exp":2},
+    "emp2":{"code":102,"name":"vipin","department":"qa","salary":27000,"exp":1},
+    "emp2":{"code":103,"name":"vishnu","department":"qa","salary":18000,"exp":2},
+    "emp2":{"code":104,"name":"hari","department":"devops","salary":24000,"exp":3},
+
+}
+
+# display employee names whose exp > 1
+print(employees.exp>1)
